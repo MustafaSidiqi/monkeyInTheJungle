30311d346d271335cde7496d9e21a65e6f9b40a2
Taras Karpin <tka@worldticket.net>
2018-08-19 22:39:19 +0200

    shake game beging

---
+++ app/src/main/java/com/example/taras/monkeyinthejungle/game_frames/ShakeGameFragment.java
@@ -0,0 +1,62 @@
+package com.example.taras.monkeyinthejungle.game_frames;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.taras.monkeyinthejungle.R;
+import com.example.taras.monkeyinthejungle.games.ShakeGame;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ShakeGameFragment extends Fragment implements Observer {
+        private static final String ARG_PARAM1 = "gameId";
+        private int gameId;
+        private View activeView;
+        ShakeGame game;
+        public ShakeGameFragment() {
+        }
+
+
+        public static com.example.taras.monkeyinthejungle.game_frames.ShakeGameFragment newInstance(int gameId) {
+            com.example.taras.monkeyinthejungle.game_frames.ShakeGameFragment fragment = new com.example.taras.monkeyinthejungle.game_frames.ShakeGameFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_PARAM1, gameId);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+                gameId = savedInstanceState.getInt(ARG_PARAM1);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            System.out.println(gameId);
+            activeView = inflater.inflate(R.layout.fragment_missing_number_game, container, false);
+            game = new ShakeGame(getActivity());
+            game.addObserver(this);
+            game.setCallBack(true);
+            game.setAlertDistance(50);
+
+            return activeView;
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            game.deleteObserver(this);
+        }
+
+        public void update(Observable obj, Object arg) {
+                TextView result = activeView.findViewById(R.id.txt_result_world_collector);
+                result.setText((String)arg);
+        }
+    }
---
+++ app/src/main/java/com/example/taras/monkeyinthejungle/games/ShakeGame.java
@@ -0,0 +1,72 @@
+package com.example.taras.monkeyinthejungle.games;
+
+import android.app.Activity;
+import android.content.Context;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+
+import java.util.Observable;
+
+
+public class ShakeGame extends Observable {
+    private SensorManager sensorManager;
+    private float shake;
+    private boolean callBackEnable;
+    private boolean alertCallBackEnable;
+    private int alertDistance;
+
+
+
+    public ShakeGame(Activity act) {
+        sensorManager = (SensorManager) act.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+        callBackEnable = false;
+        alertCallBackEnable = false;
+    }
+
+    public float getShake() {
+        return shake;
+    }
+
+    public void setAlertDistance(int distance) {
+        alertDistance = distance;
+        alertCallBackEnable = true;
+
+    }
+
+    public void setCallBack(boolean b){
+        callBackEnable = b;
+    }
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            float distance = (float) Math.sqrt(x*x+y*y+z*z) * 0.004f;
+            if(distance > 0.05) {
+                shake += distance;
+            }
+            if (callBackEnable ) {
+                setChanged();
+                notifyObservers("" + shake);
+            }
+            if(alertCallBackEnable && shake >= alertDistance) {
+                setChanged();
+                notifyObservers("done");
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+}
