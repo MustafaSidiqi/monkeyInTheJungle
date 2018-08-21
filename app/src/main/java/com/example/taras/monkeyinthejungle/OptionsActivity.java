9d5fe35f03000ee76145c11bfa2b188087b76091
Mustafa Sidiqi <mustafasidiqi95@gmail.com>
2018-08-21 11:15:05 +0200

    save settings

--- app/src/main/java/com/example/taras/monkeyinthejungle/OptionsActivity.java
+++ app/src/main/java/com/example/taras/monkeyinthejungle/OptionsActivity.java
@@ -1,27 +1,48 @@
 package com.example.taras.monkeyinthejungle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
+
 
 public class OptionsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    HashMap<String, Boolean> options_hashmap = new HashMap<String, Boolean>();
-    int numberOfRounds = 1;
+    SharedPreferences myPrefs;
+
+    private HashMap<String, Boolean> gamesToBePlayed = new HashMap<String, Boolean>();
+    private boolean randomGames;
+    private int numberOfRounds = 1;
+    private SeekBar seekBar;
 
     TextView tvProgressLabel;
     Button btn_save;
-    private SeekBar seekBar;
 
+    public HashMap<String, Boolean> getGamesToBePlayed() {
+        return gamesToBePlayed;
+    }
+
+    public boolean isRandomGames() {
+        return randomGames;
+    }
+
+    public int getNumberOfRounds() {
+        return numberOfRounds;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +82,8 @@ public class OptionsActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
+        myPrefs = getSharedPreferences("monkey", Context.MODE_PRIVATE);
+
         // initiate a Switch
         Switch two_pair_s = (Switch) findViewById(R.id.two_pair_boolean);
         Switch tap_counter_s = (Switch) findViewById(R.id.tap_counter_boolean);
@@ -75,28 +98,75 @@ public class OptionsActivity extends AppCompatActivity implements View.OnClickLi
         Boolean shake_it = shake_it_s.isChecked();
         Boolean find_the_number = find_the_number_s.isChecked();
         Boolean word_collector = word_collector_s.isChecked();
-        Boolean random_games = random_games_s.isChecked();
 
-        options_hashmap.put("two_pair", two_pair);
-        options_hashmap.put("tap_counter", tap_counter);
-        options_hashmap.put("shake_it", shake_it);
-        options_hashmap.put("find_the_number", find_the_number);
-        options_hashmap.put("word_collector", word_collector);
-        options_hashmap.put("random_games", random_games);
-
+        randomGames = random_games_s.isChecked();
         numberOfRounds = seekBar.getProgress();
 
+        gamesToBePlayed.put("two_pair", two_pair);
+        gamesToBePlayed.put("tap_counter", tap_counter);
+        gamesToBePlayed.put("shake_it", shake_it);
+        gamesToBePlayed.put("find_the_number", find_the_number);
+        gamesToBePlayed.put("word_collector", word_collector);
 
-        System.out.println();
-        System.out.println("Number Of Rounds " + numberOfRounds);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                saveMap();
+                NextIntent();
+            }
+        }, 5000); // Millisecond 1000 = 1 sec
+    }
 
-        Set set = options_hashmap.entrySet();
-        Iterator iterator = set.iterator();
+    private void NextIntent() {
+        Intent myIntent = new Intent(this, Main.class);
+        this.startActivity(myIntent);
+    }
 
-        while(iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry)iterator.next();
-            System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-            System.out.println(mentry.getValue());
+    private void saveMap(){
+        Map<String,Boolean> inputMap = getGamesToBePlayed();
+        boolean rand = isRandomGames();
+        int numOfRound = getNumberOfRounds();
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyVariables", Context.MODE_PRIVATE);
+
+        if (pref != null){
+            JSONObject jsonObject = new JSONObject(inputMap);
+            String jsonString = jsonObject.toString();
+            SharedPreferences.Editor editor = pref.edit();
+
+            editor.remove("My_map").commit();
+            editor.putString("My_map", jsonString);
+            editor.putInt("numberOfRounds", numOfRound);
+            editor.putBoolean("randomBool", rand);
+            editor.commit();
         }
+    }
+
+    private void loadMap(){
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyVariables", Context.MODE_PRIVATE);
+        gamesToBePlayed.clear();
+
+        try{
+            if (pref != null){
+                String jsonString = pref.getString("My_map", (new JSONObject()).toString());
+                JSONObject jsonObject = new JSONObject(jsonString);
+                Iterator<String> keysItr = jsonObject.keys();
+
+                while(keysItr.hasNext()) {
+                    String key = keysItr.next();
+                    Boolean value = (Boolean) jsonObject.get(key);
+                    gamesToBePlayed.put(key, value);
+                }
+
+                 randomGames = pref.getBoolean("randomBool", false);
+                 numberOfRounds = pref.getInt("numberOfRounds", 1);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("randomGames" + randomGames);
+        System.out.println("numberOfRounds" + numberOfRounds);
+        System.out.println(Arrays.asList(gamesToBePlayed)); // method 1
     }
 }
