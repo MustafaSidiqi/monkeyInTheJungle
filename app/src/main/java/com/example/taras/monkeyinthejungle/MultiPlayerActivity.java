5e68e80c306b0f6c10634d48991195dd59b5a720
Taras Karpin <tka@worldticket.net>
2018-08-22 12:49:39 +0200

    created dummy list

--- app/src/main/java/com/example/taras/monkeyinthejungle/MultiPlayerActivity.java
+++ app/src/main/java/com/example/taras/monkeyinthejungle/MultiPlayerActivity.java
@@ -1,29 +1,32 @@
 package com.example.taras.monkeyinthejungle;
 
 import android.app.ListActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MultiPlayerActivity extends ListActivity {
-
-    private View mContentView;
+public class MultiPlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player);
-        mContentView = findViewById(R.id.cns_main_multiplayer_view);
-        setFullScreenView();
-    }
 
-    private void setFullScreenView() {
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        ListView listView = (ListView) findViewById(R.id.lobby_list);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+
+        // First paramenter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
     }
 }
