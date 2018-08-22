710fd80233290d1e1d2d8cf67489ff3c72d60e6c
Taras Karpin <tka@worldticket.net>
2018-08-22 12:17:32 +0200

    created lobyDesign

--- app/src/main/java/com/example/taras/monkeyinthejungle/MultiPlayerActivity.java
+++ app/src/main/java/com/example/taras/monkeyinthejungle/MultiPlayerActivity.java
@@ -1,17 +1,12 @@
 package com.example.taras.monkeyinthejungle;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.app.ListActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.taras.monkeyinthejungle.game_frames.MissingNumberGameFragment;
-import com.example.taras.monkeyinthejungle.game_frames.ShakeGameFragment;
-import com.example.taras.monkeyinthejungle.game_frames.WordCollectorGameFragment;
-
-public class MultiPlayerActivity extends AppCompatActivity {
+public class MultiPlayerActivity extends ListActivity {
 
     private View mContentView;
 
@@ -19,33 +14,16 @@ public class MultiPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player);
-        mContentView = findViewById(R.id.frg_multi_player_game_view);
+        mContentView = findViewById(R.id.cns_main_multiplayer_view);
         setFullScreenView();
-        setFrame();
     }
 
     private void setFullScreenView() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
-    private void setFrame() {
-        MissingNumberGameFragment test = new MissingNumberGameFragment();
-        WordCollectorGameFragment test2 = new WordCollectorGameFragment();
-        ShakeGameFragment test3 = new ShakeGameFragment();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction =
-                fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frg_multi_player_game_view, test3.newInstance());
-        fragmentTransaction.commit();
     }
 }
